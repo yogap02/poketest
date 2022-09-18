8ce4119caa0649d5580300e3d3c24c9b2533b55c
yogap02 <yogalaxy69@gmail.com>
2022-09-19 00:00:05 +0700

    test(Playwright) : adding class method test and detailing on filter test.

--- test/playwright/e2e/test_landing.spec.js
+++ test/playwright/e2e/test_landing.spec.js
@@ -1,46 +1,40 @@
-import { homepage } from "../page_objects/homepage";
+const { HomePage } = require("../page_objects/homepage");
+const { test } = require("@playwright/test");
 
-const { test, expect } = require("@playwright/test");
+const pokemonElementList = [
+  "Bug",
+  "Dark",
+  "Dragon",
+  "Electric",
+  "Fairy",
+  "Fighting",
+  "Fire",
+  "Flying",
+  "Ghost",
+  "Grass",
+  "Ground",
+  "Ice",
+  "Normal",
+  "Poison",
+  "Psychic",
+  "Rock",
+  "Steel",
+  "Water",
+];
 
-function delay(time) {
-  return new Promise(function (resolve) {
-    setTimeout(resolve, time);
+test.beforeEach(async ({ page }) => {
+  const home = new HomePage(page);
+  await home.visit();
+});
+
+test("Check Search", async ({ page }) => {
+  const home = new HomePage(page);
+  await home.search("Pidgey", "Flying");
+});
+
+for (let element in pokemonElementList) {
+  test(`Check Filter Pokemon Type with value ${pokemonElementList[element]}`, async ({ page }) => {
+    const home = new HomePage(page);
+    await home.filterType(pokemonElementList[element]);
   });
 }
-
-test("Check Search Feature", async ({ page }) => {
-  const home = new homepage(page);
-  await page.goto("/");
-  await expect(page).toHaveTitle(home.title);
-  await home.searchBar.click();
-  await home.searchBar.fill("Pikachu");
-  const locator = page.locator('a[class*="pokemon-card"]').first();
-  await expect(locator, "Pikachu is shown up").toContainText("Pikachu", {
-    timeout: 5000,
-  });
-  await expect(locator, "Pikachu included in electric pokemon").toContainText(
-    "electric",
-    { timeout: 5000 }
-  );
-  await locator.click();
-  const detailName = page.locator("h1");
-  await expect(detailName, "Detail card have Pikachu's name").toHaveText(
-    "Pikachu",
-    { timeout: 5000 }
-  );
-  const image = page.locator('img[alt="Pikachu"]');
-  await expect(image, "Image is showing alt text of Pikachu").toBeVisible();
-});
-
-test("Check Filter Feature", async ({ page }) => {
-  const home = new homepage(page);
-  await page.goto("/");
-  await expect(page).toHaveTitle(home.title);
-  await page.selectOption('select[class*="mx-px"] >> nth=-1', { label: "Bug" });
-  const cardList = page.locator('a[class*="pokemon-card"]')
-  const totalCount = await cardList.count()
-  await delay(2000);
-  for (let a = 0; a < totalCount; a++) {
-    await expect(page.locator('a[class*="pokemon-card"]').nth(a), `Card #${a} is contain bug type pokemon`).toContainText("bug", { timeout: 5000 });
-  }
-});
--- test/playwright/page_objects/homepage.js
+++ test/playwright/page_objects/homepage.js
@@ -1,9 +1,50 @@
-export class homepage {
+import { delay } from "../helper/helper";
 
-    constructor(page) {
-        this.page = page;
-        this.title = 'Pokémon Awesome';
-        this.searchBar = page.locator('input[placeholder="🔍 Search pokémon"]');
+const { expect } = require('@playwright/test');
+
+exports.HomePage = class HomePage {
+
+  constructor(page) {
+    this.page = page;
+    this.title = "Pokémon Awesome";
+    this.detailName = page.locator("h1");
+    this.searchBar = page.locator('input[placeholder="🔍 Search pokémon"]');
+    this.pokemonCards = page.locator('a[class*="pokemon-card"]');
+    this.nameOnCards = page.locator('a[class*="pokemon-card"] >> b[class*="text-xl"]');
+  }
+
+  async visit() {
+    await this.page.goto("/");
+    await expect(this.page).toHaveTitle(this.title);
+  }
+
+  async search(pokemonName, pokemonElement) {
+    console.log(`Execute search with pokemon name ${pokemonName} with element ${pokemonElement}`)
+    pokemonElement = pokemonElement.toLowerCase();
+    await delay(2000);
+    await this.searchBar.click();
+    await this.searchBar.fill(pokemonName);
+    const locator = this.pokemonCards.first();
+    await expect(locator, `${pokemonName} is shown up`).toContainText(pokemonName,{timeout: 5000,});
+    await expect(locator,`${pokemonName} included in ${pokemonElement} pokemon`).toContainText(pokemonElement, { timeout: 5000 });
+    await locator.click();
+    await expect(this.detailName, `Detail card have ${pokemonName}'s name`).toHaveText(pokemonName, { timeout: 5000 });
+    const image = this.page.locator(`img[alt=${pokemonName}]`);
+    await expect(image, `Image is showing alt text of ${pokemonName}`).toBeVisible();
+  }
+
+  async filterType(pokemonType) {
+    console.log(`Execute function Filter Type with value ${pokemonType}`)
+    await delay(2000);
+    await this.page.selectOption('select[class*="mx-px"] >> nth=-1', { label: pokemonType });
+    pokemonType = pokemonType.toLowerCase()
+    const totalCount = await this.pokemonCards.count()
+    await delay(1000);
+    for (let index = 0; index < totalCount; index++) {
+      let target = await this.pokemonCards.nth(index)
+      let textOnCard = await target.innerText()
+      let nameOfPokemon = await textOnCard.split('\n')[0]
+      await expect(target, `Card #${index}, ${nameOfPokemon} is a ${pokemonType} type Pokemon`).toContainText(pokemonType, { timeout: 5000 });
     }
-
+  }
 }
